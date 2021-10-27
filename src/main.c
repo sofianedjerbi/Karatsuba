b4ec22eabbaf48e380bc07c093167e40b9dade31
Kugge <sofiane.djerbi38@gmail.com>
2021-10-28 01:00:15 +0200

    Changed defaults

--- src/main.c
+++ src/main.c
@@ -8,27 +8,5 @@
 int main(int argc, char *argv[]) {
     
     printf("Test mulpu:\n");
-    test_multiply(mulpu, 100000, 1000);
-    /*
-    poly_u *p = alloc_poly_u(2);
-    
-    p->coef[0] = 3;
-    p->coef[1] = 2;
-    p->coef[2] = 3;
-    
-    poly_u *q = alloc_poly_u(2);
-    q->coef[0] = 1;
-    q->coef[1] = 2;
-    q->coef[2] = 3;
-    
-    poly_u *k = mulpu(p, q);
-    
-    print_poly_u(k);
-    print_poly_u(p);
-        
-    free_poly_u(p);
-    free_poly_u(q);
-    free_poly_u(k);
-    */
+    test_multiply(mulpu, 100000, 10000);
 }
-
