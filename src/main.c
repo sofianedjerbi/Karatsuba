cce219f5dd780e9cd6a0de064ba3fc4cdd53ea0b
kugge <sofiane.djerbi38@gmail.com>
2021-10-29 21:27:25 +0200

    All commands added

--- src/main.c
+++ src/main.c
@@ -10,28 +10,26 @@
 void print_options(){
     printf("Options:\n");
     printf("    -n: Naive multiplication (mulpu)\n");
+    printf("    -k1: 1 decomposition multiplication (mulpk1)\n");
+    printf("    -k: recursive decomposition multiplication (mulpkr)\n");
+    printf("    -t: tests\n");
 }
 
 int main(int argc, char **argv) {
-    poly_u *q = alloc_poly_u(5);
-    poly_u *p = alloc_poly_u(4);
-    uint32_t c[] = {0,1,2,3,4};
-    uint32_t d[] = {0,1,1,1,1,1};
-    p->coef = c;
-    q->coef = d;
-    poly_u *g = addpu(p,q);
-    print_poly_u(p);
-    print_poly_u(q);
-    print_poly_u(g);
-    
-    /*
     if (argc != 2)
         print_options();
-
-    else if (strcmp(argv[1], "-n") == 0)
-        test_multiply(mulpu, 20000, 1000);
-    
+    else if (strcmp(argv[1], "-n") == 0)  // mulpu
+        test_multiply(mulpuk1, 200000, 10000);
+    else if (strcmp(argv[1], "-k") == 0) // mulpuk1
+        test_multiply(mulpukr, 200000, 10000);
+    else if (strcmp(argv[1], "-k1") == 0) // mulpuk1
+        test_multiply(mulpuk1, 200000, 10000);
+    else if (strcmp(argv[1], "-t") == 0){ // Tests
+        compare_multiply(mulpuk1); // Make sure that mulpuk1 works
+        compare_multiply(mulpukr);
+    }
+    else
+        print_options();
     return 0;
-    */
 }
 
