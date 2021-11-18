ae83d89b7f5840f577098931dac3b5b4fb730c24
kugge <sofiane.djerbi38@gmail.com>
2021-11-18 16:07:09 +0100

    Fixed tests for low-end processors

--- src/main.c
+++ src/main.c
@@ -19,16 +19,16 @@ int main(int argc, char **argv) {
     if (argc != 2)
         print_options();
     else if (strcmp(argv[1], "-n") == 0)  // mulpu
-        test_multiply(mulpu, 500000, 100000);
+        test_multiply(mulpu, 100000, 10000);
     else if (strcmp(argv[1], "-k") == 0)  // mulpuk1
-        test_multiply(mulpukr, 500000, 100000);
+        test_multiply(mulpukr, 100000, 10000);
     else if (strcmp(argv[1], "-k1") == 0) // mulpuk1
-        test_multiply(mulpuk1, 500000, 100000);
+        test_multiply(mulpuk1, 100000, 10000);
     else if (strcmp(argv[1], "-t") == 0){ // Tests
         printf("mulpuk1 test\n");
         compare_multiply(mulpuk1); // Make sure that mulpuk1 works
         printf("\nmulpukrt test\n");
-        //compare_multiply(mulpukr); // mulpukr
+        compare_multiply(mulpukr); // mulpukr
     }
     else
         print_options();
